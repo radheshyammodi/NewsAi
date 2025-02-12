3de6e78b1d1026d21cfaa8361390168417581f06
Radheshyam <modimayank786@gmail.com>
2025-02-12 18:36:00 +0530

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500 font-bold'>App</h1>
+    </div>
+  )
+}
---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import dbConnect from "./config/db.js";
+
+const app = express();
+dotenv.config();
+
+dbConnect();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on the Port ${process.env.PORT}`);
+});
